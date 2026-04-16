50a9105a418e21464719a2705437306a4ebe0628
Yash Vaidya <vaidyayash974@gmail.com>
2026-04-16 20:47:44 +0530

    Implement video feed from user media

--- ai-smart-security-camera/frontend/src/components/VideoFeed.js
+++ ai-smart-security-camera/frontend/src/components/VideoFeed.js
@@ -1,1 +1,14 @@
+import React, { useEffect, useRef } from "react";
 
+export default function VideoFeed() {
+  const videoRef = useRef();
+
+  useEffect(() => {
+    navigator.mediaDevices.getUserMedia({ video: true })
+      .then(stream => {
+        videoRef.current.srcObject = stream;
+      });
+  }, []);
+
+  return <video ref={videoRef} autoPlay width="500" />;
+}
