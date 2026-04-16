08b0cd33c414445870e4c4fa5eeae8e979cae4dc
Yash Vaidya <vaidyayash974@gmail.com>
2026-04-16 20:34:04 +0530

    Implement object detection in detector.py
    Added detection logic to process frames and extract bounding box information.

--- ai-smart-security-camera/backend/app/core/detector.py
+++ ai-smart-security-camera/backend/app/core/detector.py
@@ -1,1 +1,17 @@
+from app.core.model import get_model
 
+def detect(frame):
+  model = get_model()
+  result = model(frame)
+  detections = []
+
+for box in results[0].boxes.data.tolist():
+  detections.append({
+    "x1" : box[0],
+    "y1" : box[1],
+    "x2" : box[2],
+    "y2" : box[3],
+    "confidence" : box[4]
+    "class" : int(box[5])
+  })
+  return detections
