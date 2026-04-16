b84cd2c5acbf8367a00f7325fc7c4929ece66c04
Yash Vaidya <vaidyayash974@gmail.com>
2026-04-16 20:42:16 +0530

    Add detection API endpoint to router

--- ai-smart-security-camera/backend/app/api/router.py
+++ ai-smart-security-camera/backend/app/api/router.py
@@ -1,1 +1,14 @@
+from faseapi import APIRouter
+from app.services.camera import get_frame
+from app.core.detector import detect
 
+router = APIRouter()
+
+@router.get("/detect")
+def detect_api():
+  frame = get_frame()
+  if frame is None:
+    return{"error" : "Camera Not Working"}
+  detections = detect(frame)
+  return{"detections" : detections}
+    
